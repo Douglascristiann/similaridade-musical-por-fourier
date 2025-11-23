7f77b5105a3f13d1716aa27efcfd952b6a1ac011
Douglas Cristian Da Cunha <69438495+Douglascristiann@users.noreply.github.com>
2025-11-23 00:12:58 -0300

    update start bot shell

---
+++ start_bot.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+pip install -U yt-dlp -U
+export SPOTIFY_CLIENT_SECRET='af5d7ad2ab994da6be5fcede295d0063'
+export SPOTIFY_MARKET='BR'
+export SPOTIFY_CLIENT_ID='1dd34075737b446886f82875bcb76dff'
+export BOT_TOKEN=8174549763:AAGH5Fl_Klkn6J_SfJWgob_F0Lv4kDSitAw
+
+python -m app_v5.integrations.menu_bot
